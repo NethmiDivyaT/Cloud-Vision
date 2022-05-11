9e4110e53bcea665e4fbff7739cf50a98b2551ab
NethmiDivyaT <nethmitharushika1999@gmail.com>
2022-05-11 14:52:42 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
+from flask import Flask, request, render_template
 from google.cloud import vision
-from flask import render_template, Flask, request
+from google.cloud import storage
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('main.html')
+    return render_template("main.html")
 
 
-@app.route('/display',  methods=['GET', 'POST'])
-def display():
+@app.route("/display", methods=["GET", "POST"])
+def speech():
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "auth.json"
 
-    # Imports the Google Cloud client library
-    from google.cloud import vision
+    bucket_name = "lively-metrics-337209.appspot.com"
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
 
-    # Instantiates a client
     client = vision.ImageAnnotatorClient()
 
-    # Performs label detection on the image file
-    response = client.label_detection(
-        {
-            "source": {
-                "image_uri": "gs://lively-metrics-337209.appspot.com/humans-2.jpeg"
-            },
-        }
-    )
-    labels = response.label_annotations
+    dog = []
+    cat = []
 
-    allDescreiptios = []
+    for blob in blobs:
 
-    for label in labels:
-        allDescreiptios.append(
-            {label.description: str(round(label.score * 100, 2))})
+        image_file = f'gs://lively-metrics-337209.appspot.com/{blob.name}'
 
-    return render_template("main.html", transcript=allDescreiptios)
+        objects = client.object_localization(
+            {
+                "source": {
+                    "image_uri": image_file
+                },
+            }
+        ).localized_object_annotations
+
+        for object_ in objects:
+
+            if str(object_.name) == 'dog' or str(object_.name) == 'Dog' or str(object_.name) == 'DOG':
+                dog.append(blob.name)
+
+            if str(object_.name) == 'cat' or str(object_.name) == 'Cat' or str(object_.name) == 'CAT':
+                cat.append(blob.name)
+
+    return render_template("main.html", cat=cat, dog=dog)
 
 
 if __name__ == "__main__":
